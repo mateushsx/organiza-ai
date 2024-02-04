a5dd88258647f115c36f15991ff432b5169a2879
Mateus Henrique <mateusdzn10@gmail.com>
2024-02-04 16:33:53 -0400

    feat: add route to update task

--- backend/src/routes/task.ts
+++ backend/src/routes/task.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { createTaskController, listTasksController } from '~/controllers/task';
+import {
+  createTaskController,
+  listTasksController,
+  updateTaskController,
+} from '~/controllers/task';
 
 const taskRoutes = Router();
 
 taskRoutes.post('/task/create', createTaskController);
 taskRoutes.post('/task/list', listTasksController);
+taskRoutes.put('/task/update', updateTaskController);
 
 export { taskRoutes };
